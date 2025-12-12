1c94192a2b9dbc0bc654ad2d42b4eabe9dccda8a
Olivier Delmas <olivier.delmas@edu.devinci.fr>
2025-12-12 15:03:12 +0100

    Revert "feat: added sentry"
    This reverts commit b92d6d1770d87eaa67798a820e08d29577b9cf4f.

--- packages/server/instrument.mjs
+++
@@ -1,8 +0,0 @@
-import * as Sentry from "@sentry/node"
-
-Sentry.init({
-    dsn: "https://0a41e1a9932b6ea3e7470635d0137cf5@o4510522032586752.ingest.de.sentry.io/4510522034094160",
-    // Setting this option to true will send default PII data to Sentry.
-    // For example, automatic IP address collection on events
-    sendDefaultPii: true,
-});
